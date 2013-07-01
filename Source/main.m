0e458e4e71558828bee070c84a4b3a740a06fd55
Mento <Mento@gpgtools.org>
2013-07-01 13:59:29 +0200

    Message if Libmacgpg was not foud.

--- Source/main.m
+++ Source/main.m
@@ -1,5 +1,12 @@
 
+#import <Libmacgpg/Libmacgpg.h>
+#import "Globales.h"
 
 int main(int argc, char *argv[]) {
+	if (![GPGController class]) {
+		NSRunAlertPanel(localized(@"LIBMACGPG_NOT_FOUND_TITLE"), localized(@"LIBMACGPG_NOT_FOUND_MESSAGE"), nil, nil, nil);
+		return 1;
+	}
+
     return NSApplicationMain(argc,  (const char **) argv);
 }
