470d6e821554f6e6f240b546b4f6f59e27c4e45d
Mento <Mento@gpgtools.org>
2015-01-14 15:14:38 +0100

    Fix the Dock icon.
    Change an existing GKA Dock icon, so it point's to the new GPG
    Keychain.app
    [#288 state:fixed assigned:mento]

--- Installer/postinstall.sh
+++ Installer/postinstall.sh
@@ -22,4 +22,76 @@ mv "$tempLocation" "$installLocation" || exit 1
 chmod -R u=rwX,go=rX "$installLocation"
 
 
+
+
+########################################################################################
+#####     Code to fix the Dock icon     ################################################
+########################################################################################
+
+DOCK_ID=com.apple.dock
+DOCK_PLIST=~/Library/Preferences/$DOCK_ID.plist
+PLIST_BUDDY=/usr/libexec/PlistBuddy
+GK_ID_OLD=org.gpgtools.gpgkeychainaccess
+GK_ID=org.gpgtools.gpgkeychain
+
+# Fetch the number of dock items.
+NR_OF_DOCK_ITEMS=$(defaults read $DOCK_ID persistent-apps | grep -F '_CFURLString"' | wc -l)
+#
+# Find the position of the old dock GPG Keychain Access item
+GK_INDEX=-1
+# Loop through the items to find the index for GPG Keychain.
+for((i = 0; i < $NR_OF_DOCK_ITEMS; i++)); do
+	# Search for the item with the bundle-identifier $GK_ID_OLD.
+    if [[ "$($PLIST_BUDDY -c "Print persistent-apps:$[$i]:tile-data:bundle-identifier" $DOCK_PLIST)" == "$GK_ID_OLD" ]] ;then
+		# Found it.
+		GK_INDEX=$i
+		break
+	fi
+done
+
+
+echo "$GK_INDEX"
+
+if [[ "$GK_INDEX" == "-1" ]]; then
+	echo "No dock item found. Continue..."
+	exit 0
+fi
+
+# Check the bundle ID to be sure, to have the right application.
+GK_PATH=$($PLIST_BUDDY -c "Print persistent-apps:$[$GK_INDEX]:tile-data:file-data:_CFURLString" $DOCK_PLIST)
+if [[ "${GK_PATH:0:7}" == 'file://' ]] ;then
+	GK_PATH=$(perl -MURI -le 'print URI->new(<>)->file' <<<"$GK_PATH")
+fi
+BUNDLE_ID=$($PLIST_BUDDY -c "Print CFBundleIdentifier" "$GK_PATH/Contents/Info.plist")
+if [[ "$BUNDLE_ID" != "$GK_ID_OLD" ]] ;then
+	echo "Bundle ID doesn't match. Continue..."
+	exit 0
+fi
+
+
+# Use the installLocation as in postinstall and set it as the new location.
+installLocation=$(mdfind -onlyin /Applications "kMDItemCFBundleIdentifier = org.gpgtools.gpgkeychain" | head -1)
+NEW_PATH=${installLocation:-/Applications/GPG Keychain.app}
+
+
+# Update the CFURLString to the new location
+$PLIST_BUDDY -c "Set persistent-apps:$[$GK_INDEX]:tile-data:file-data:_CFURLString $NEW_PATH" $DOCK_PLIST
+# Update the bundle identifier.
+$PLIST_BUDDY -c "Set persistent-apps:$[$GK_INDEX]:tile-data:bundle-identifier $GK_ID" $DOCK_PLIST
+# Remove parent-mod-date, file-data:_CFURLAliasData, file-mod-date in order to have them recreated.
+# Otherwise the dock item will still point to the old path.
+$PLIST_BUDDY -c "Delete persistent-apps:$[$GK_INDEX]:tile-data:file-data:_CFURLAliasData" $DOCK_PLIST 2> /dev/null
+$PLIST_BUDDY -c "Delete persistent-apps:$[$GK_INDEX]:tile-data:parent-mod-date" $DOCK_PLIST 2> /dev/null
+$PLIST_BUDDY -c "Delete persistent-apps:$[$GK_INDEX]:tile-data:file-mod-date" $DOCK_PLIST 2> /dev/null
+# Remove the label, otherwise the old name would appear.
+$PLIST_BUDDY -c "Delete persistent-apps:$[$GK_INDEX]:tile-data:file-label" $DOCK_PLIST 2> /dev/null
+
+# Read the dock plist to make sure the cache is flushed (otherwise our changes are not reflected)
+defaults read $DOCK_ID > /dev/null
+# Kill the dock to complete the fix.
+killall Dock
+
+########################################################################################
+
+
 exit 0
