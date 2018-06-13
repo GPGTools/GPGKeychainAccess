GPGTools/GPGKeychainAccess

commit b354238854cd74580de6592e3ade02ef5c7597ce

Unify signing proceedure
[#282 state:fixed milestone:1.4.3]

Source/Globales.m
@@ -1,5 +1,5 @@
 /*
- Copyright © Roman Zechmeister, 2017
+ Copyright © Roman Zechmeister, 2018
  
  Diese Datei ist Teil von GPG Keychain.
  
@@ -151,10 +151,24 @@ NSString *filenameForExportedKeys(NSArray *keys, NSString **secFilename) {
 @implementation GKFingerprintTransformer
 - (id)transformedValue:(id)value {
 	NSString *fingerprint = [value description];
+	NSString *transformed;
 	if (fingerprint.length <= 16) {
-		return @"0000 0000 0000 0000  0000 0000 0000 0000";
+		transformed = @"0000 0000 0000 0000  0000 0000 0000 0000";
+	} else {
+		transformed = [super transformedValue:value];
 	}
-	return [super transformedValue:value];
+	
+	transformed = [transformed stringByReplacingOccurrencesOfString:@"  " withString:@"\xC2\xA0"];
+	transformed = [transformed stringByReplacingOccurrencesOfString:@" " withString:@"\xE2\x80\xAF"];
+	return transformed;
+}
++ (id)sharedInstance {
+	static dispatch_once_t onceToken = 0;
+	__strong static id _sharedInstance = nil;
+	dispatch_once(&onceToken, ^{
+		_sharedInstance = [[self alloc] init];
+	});
+	return _sharedInstance;
 }
 @end
 

Resources/Base.lproj/MainMenu.xib
@@ -2538,7 +2538,7 @@ CA
                                 </connections>
                             </menuItem>
                             <menuItem isSeparatorItem="YES" id="1864"/>
-                            <menuItem title="Sign…" keyEquivalent="s" id="1597">
+                            <menuItem title="Sign…" tag="-1" keyEquivalent="s" id="1597">
                                 <modifierMask key="keyEquivalentModifierMask" option="YES" command="YES"/>
                                 <connections>
                                     <action selector="addSignature:" target="-1" id="1631"/>

Resources/en.lproj/Localizable.strings
@@ -207,6 +207,10 @@ RevokedSubkeyWantToUpload_Msg = "To make it easier for your friends and colleagu
 RevokedSubkeyWantToUpload_Yes = "Upload Public Key";
 RevokedSubkeyWantToUpload_No = "No, Thanks!";
 
+UserIDsSignedWantToUpload_Title = "Signing successful";
+UserIDsSignedWantToUpload_Msg = "To make it easier for your friends to learn about the new signature, it is recommended to upload the signed public key to the key servers.\n\nDo you want to upload your public key?";
+UserIDsSignedWantToUpload_Yes = "Upload Public Key";
+UserIDsSignedWantToUpload_No = "No, Thanks!";
 
 
 
@@ -261,6 +265,10 @@ UploadSuccess_Msg = "The following public keys have been sent to the key servers
 
 ExportSuccess_Title = "Export successful";
 ExportSuccess_Msg = "The following keys were successfully exported:\n\n%@";
+
+SignSuccess_Title = "Signing successful";
+SignSuccess_Msg = "The selected user IDs have been signed.";
+
 
 
 /* For Import-Results */
@@ -397,4 +405,13 @@ SendPublicKeyToKeyserver_MenuItem = "Send Public Key to Key Server";
 
 FilterNoResults = "The local search for '%@' has no results.";
 FilterNoResultsSecOnly = "The local search for '%@' has no results.\nConsider disabling the option to 'Show secret keys only' to expand the search.";
+
+
+
+SignKey_KeyClaimsMultipleIdentities = "The key:\n%@\nclaims more than one identity. Which ones have you verified?";
+SignKey_PublishSingleIdentity = "If you choose to publish this signature, everyone in the world can see that you believe this key belongs to %@.";
+SignKey_PublishMultipleIdentities = "If you choose to publish this signature, everyone in the world can see that you believe this key belongs to the identities you've checked.";
+SignKey_MainMsg = "By signing this key you confirm the identity of the key owner and that the key with fingerprint %@ belongs to them.";
+
+
 

Resources/en.lproj/ModalSheets.strings
@@ -13,20 +13,13 @@
 "254.title" = "Elgamal (encrypt only)";
 "255.title" = "RSA (sign only)";
 "256.title" = "DSA (sign only)";
-"378.title" = "Generate Signature";
+"378.title" = "Sign";
 "379.title" = "Cancel";
-"381.title" = "Expiration date:";
-"389.title" = "I will not answer.";
 "392.title" = "Signature expires";
-"405.title" = "I have not checked at all.";
-"406.title" = "I have done casual checking.";
-"407.title" = "I have done very careful checking.";
-"409.title" = "How carefully have you verified, that the key you are about to sign actually belongs to the person named above?";
 "427.title" = "Change Expiration Date";
 "428.title" = "Cancel";
 "430.title" = "Expiration date:";
 "441.title" = "Key expires";
-"468.title" = "Local signature";
 "481.title" = "Search";
 "482.title" = "Cancel";
 "485.title" = "Search for public key";
@@ -69,8 +62,12 @@
 "1095.title" = "Cancel";
 "1153.ibExternalAccessibilityDescription" = "Advanced options";
 "1156.title" = "Advanced options";
+"2JB-h4-xn9.title" = "Example: You signed Bob's key. Your friend Alice finds Bob's key on the key server, but she is unsure if the found key is really Bob's key. Alice sees your signature on Bob's key and since Alice knows the fingerprint of your key is trustworthy, she assumes that the found key for Bob is indeed his key.";
 "8NR-sq-tyn.title" = "Move now";
+"9R1-eW-2wX.title" = "Your secret key used to sign:";
+"D2R-cV-2w2.title" = "This key claims more than one identity. Which ones have you verified?";
+"Ebm-sP-X8A.title" = "Why Publish?";
 "FWA-34-yOE.title" = "Cancel";
 "FsT-ha-Sbf.ibShadowedIsNilPlaceholder" = "Search for name, email or fingerprint.";
+"J0P-gR-G8d.title" = "Publish";
 "c3R-Qv-JF7.title" = "GPG Keychain will move your secret keys to the selected location. gpg will find your keys as soon as the target volume is connected to this machine.\n\nIMPORTANT: After moving your secret keys, you will always need the new target volume to sign or decrypt mails or files.";
-"eKE-tf-ktq.title" = "Do you want your signature to expire (recommended)?";

Source/ActionController.h
@@ -1,5 +1,5 @@
 /*
- Copyright © Roman Zechmeister, 2017
+ Copyright © Roman Zechmeister, 2018
  
  Diese Datei ist Teil von GPG Keychain.
  
@@ -21,6 +21,16 @@
 #import "GKMenuButton.h"
 
 @class SheetController;
+
+typedef enum {
+	DescriptionNoKeyID = 8,
+	DescriptionNoEmail = 16,
+	DescriptionNoName = 32,
+	DescriptionSingleLine = 64,
+	DescriptionIndent = 128,
+	DescriptionFingerprint = 256
+	
+} DescriptionOptions;
 
 
 @interface ActionController : NSWindowController <GPGControllerDelegate, NSPopoverDelegate, GKMenuButtonDelegate> {
@@ -82,5 +92,6 @@
 - (IBAction)sendKeysPerMail:(id)sender;
 - (BOOL)validateUserInterfaceItem:(id)anItem;
 
+- (NSString *)descriptionForKeys:(NSObject <EnumerationList> *)keys maxLines:(NSUInteger)lines withOptions:(DescriptionOptions)options;
 
 @end

Source/ActionController.m
@@ -1,5 +1,5 @@
 /*
- Copyright © Roman Zechmeister, 2017
+ Copyright © Roman Zechmeister, 2018
  
  Diese Datei ist Teil von GPG Keychain.
  
@@ -1928,63 +1928,133 @@ static NSString * const actionKey = @"action";
 		return;
 	}
 	
-	GPGUserID *userID = nil;
-	if ([sender tag] == 1) {
-		NSArray *objects = [self selectedObjectsOf:userIDsTable];
-		if (objects.count != 1) {
-			return;
-		}
-		userID = [objects objectAtIndex:0];
-	}
-	
 	GPGKey *key = [keys[0] primaryKey];
 	if (key.validity >= GPGValidityInvalid) {
+		// Only valid keys can be signed.
 		return;
 	}
+
+	GPGUserID *userID = nil;
+	if ([sender tag] == 1 || ([sender tag] == -1 && (mainWindow.firstResponder == userIDsTable || mainWindow.firstResponder == signaturesTable))) {
+		NSArray *objects = [self selectedObjectsOf:userIDsTable];
+		if (objects.count == 1) {
+			// The user has selected a userID in the user ids tab.
+			// This userID will be pre-selected.
+			userID = objects[0];
+		}
+	}
 	
+	
+	// Get a sorted list of all secret keys, which could be used for signing.
 	NSSet *usableSecretKeys = [[KeychainController sharedInstance].secretKeys objectsPassingTest:^BOOL(GPGKey *secretKey, BOOL *stop) {
 		return secretKey.validity < GPGValidityInvalid && secretKey.canAnySign;
 	}];
-	
 	if (usableSecretKeys.count == 0) {
 		[self.sheetController errorSheetWithMessageText:localized(@"NO_SECRET_KEY_TITLE") infoText:localized(@"NO_SECRET_KEY_MESSAGE")];
 		return;
 	}
-
 	NSSortDescriptor *descriptor = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
 	NSArray *secretKeys = [usableSecretKeys sortedArrayUsingDescriptors:@[descriptor]];
 
-	
+
 	GPGKey *defaultKey = [KeychainController sharedInstance].defaultKey;
-	if (!defaultKey) {
+	if (!defaultKey || ![secretKeys containsObject:defaultKey]) {
+		// If the user haven't specified a default key, the first secret key is used.
 		defaultKey = secretKeys[0];
 	}
 	
 	
-	NSString *msgText;
-	if (userID) {
-		msgText = [NSString stringWithFormat:localized(@"GenerateUidSignature_Msg"), [NSString stringWithFormat:@"%@ (%@)", userID.userIDDescription, key.keyID.shortKeyID]];
-	} else {
-		msgText = [NSString stringWithFormat:localized(@"GenerateSignature_Msg"), key.userIDAndKeyID];
-	}
-	
-	self.sheetController.msgText = msgText;
 	self.sheetController.secretKeys = secretKeys;
 	self.sheetController.secretKey = defaultKey;
+	self.sheetController.publicKey = key;
+	self.sheetController.selectedUserIDs = userID ? @[userID] : nil;
+	self.sheetController.msgText = localizedStringWithFormat(@"GenerateSignature_Msg", key.userIDAndKeyID);
 	
+	
+	
+	__block int64_t runningTasks = 2;
+	__block BOOL keyExistsOnServer = NO;
+	void (^uploadBlock)() = ^() {
+		if (OSAtomicAdd64Barrier(-1, &runningTasks) == 0) {
+			// Run this code when uploadBlock is called the second time.
+			
+			if (keyExistsOnServer) {
+				if ([self warningSheetWithDefault:YES string:@"UserIDsSignedWantToUpload"]) {
+					self.progressText = localizedStringWithFormat(@"SendKeysToServer_Progress", [self descriptionForKey:key]);
+					self.errorText = localized(@"SendKeysToServer_Error");
+					[gpgc sendKeysToServer:@[key]];
+				}
+			} else {
+				[self.sheetController alertSheetWithTitle:localized(@"SignSuccess_Title")
+												  message:localized(@"SignSuccess_Msg")
+											defaultButton:nil
+										  alternateButton:nil
+											  otherButton:nil
+										suppressionButton:nil];
+			}
+		}
+	};
+	// Check if the key already exists on the server and only ask for upload after signing, if that's the case.
+	[gpgc keysExistOnServer:@[key] callback:^(NSArray *existingKeys, NSArray *nonExistingKeys) {
+		keyExistsOnServer = existingKeys.count == 1;
+		uploadBlock();
+	}];
+	actionCallback callback = [^(GPGController *gc, id value, NSDictionary *userInfo) {
+		if (gc.error) {
+			[self.sheetController endProgressSheet];
+			return;
+		}
+		if (self.sheetController.publish) {
+			uploadBlock();
+		} else {
+			// The user do not want to publish the signature.
+			// Do not offer upload, only show success message.
+			[self.sheetController alertSheetWithTitle:localized(@"SignSuccess_Title")
+											  message:localized(@"SignSuccess_Msg")
+										defaultButton:nil
+									  alternateButton:nil
+										  otherButton:nil
+									suppressionButton:nil];
+		}
+	} copy];
+	
+	
+	// Show the sign dialog.
 	self.sheetController.sheetType = SheetTypeAddSignature;
 	if ([self.sheetController runModalForWindow:mainWindow] != NSOKButton) {
 		return;
 	}
 	
+	// Get the selected userIDs. Only procced if the user selected a userID.
+	NSArray *userIDs = self.sheetController.selectedUserIDs;
+	if (userIDs.count == 0) {
+		return;
+	}
+	
+	
 	self.progressText = localized(@"AddSignature_Progress");
 	self.errorText = localized(@"AddSignature_Error");
-	[gpgc signUserID:[userID hashID]
-			   ofKey:key
-			 signKey:self.sheetController.secretKey
-				type:(int)self.sheetController.sigType
-			   local:self.sheetController.localSig
-		daysToExpire:(int)self.sheetController.daysToExpire];
+	[self showProgressUntilKeyIsRefreshed:key];
+
+	
+	gpgc.userInfo = @{@"action": @[callback]};
+	
+	if ([gpgc respondsToSelector:@selector(signUserIDs:signerKey:local:daysToExpire:)]) {
+		[gpgc signUserIDs:userIDs
+				signerKey:self.sheetController.secretKey
+					local:!self.sheetController.publish
+			 daysToExpire:(int)self.sheetController.daysToExpire];
+	} else {
+		// This is only a workaround for old Libmacgpg versions.
+		for (GPGUserID *uid in userIDs) {
+			[gpgc signUserID:uid.hashID
+					   ofKey:key
+					 signKey:self.sheetController.secretKey
+						type:0
+					   local:!self.sheetController.publish
+				daysToExpire:(int)self.sheetController.daysToExpire];
+		}
+	}
 }
 - (IBAction)removeSignature:(id)sender {
 	NSArray *objects = [self selectedObjectsOf:signaturesTable];
@@ -2644,6 +2714,7 @@ static NSString * const actionKey = @"action";
 	NSMutableString *descriptions = [NSMutableString string];
 	Class gpgKeyClass = [GPGKey class];
 	Class dictionaryClass = [NSDictionary class];
+	Class userIDClass = [GPGUserID class];
 	NSUInteger i = 0, count = keys.count;
 	if (count == 0) {
 		return @"";
@@ -2657,6 +2728,7 @@ static NSString * const actionKey = @"action";
 	BOOL showEmail = !(options & DescriptionNoEmail);
 	BOOL singleLine = options & DescriptionSingleLine;
 	BOOL indent = options & DescriptionIndent;
+	BOOL showFingerprint = !!(options & DescriptionFingerprint);
 	
 	NSString *lineBreak = indent ? @"\n\t" : @"\n";
 	if (indent) {
@@ -2673,7 +2745,7 @@ static NSString * const actionKey = @"action";
 			break;
 		}
 
-		if (![key isKindOfClass:gpgKeyClass] && ![key isKindOfClass:dictionaryClass]) {
+		if (![key isKindOfClass:gpgKeyClass] && ![key isKindOfClass:dictionaryClass] && ![key isKindOfClass:userIDClass]) {
 			GPGKeyManager *keyManager = [GPGKeyManager sharedInstance];
 			GPGKey *realKey = [[keyManager allKeysAndSubkeys] member:key];
 			
@@ -2693,11 +2765,18 @@ static NSString * const actionKey = @"action";
 		}
 		
 		
+		BOOL isUserID = [key isKindOfClass:userIDClass];
 		
-		if ([key isKindOfClass:gpgKeyClass] || [key isKindOfClass:dictionaryClass]) {
+		if ([key isKindOfClass:gpgKeyClass] || [key isKindOfClass:dictionaryClass] || isUserID) {
 			NSString *name = [key valueForKey:@"name"];
 			NSString *email = [key valueForKey:@"email"];
-			NSString *shortKeyID = [[key valueForKey:@"keyID"] shortKeyID];
+			NSString *keyID;
+			if (showFingerprint) {
+				keyID = isUserID ? [(GPGUserID *)key primaryKey].fingerprint : [key valueForKey:@"fingerprint"];
+				keyID = [[GKFingerprintTransformer sharedInstance] transformedValue:keyID];
+			} else {
+				keyID = isUserID ? [(GPGUserID *)key primaryKey].keyID : [key valueForKey:@"keyID"];
+			}
 			
 			NSUInteger mailFlag = 0;
 			if (name.length == 0) {
@@ -2719,20 +2798,20 @@ static NSString * const actionKey = @"action";
 					[descriptions appendFormat:@"%@%@ <%@>", seperator, name, email];
 					break;
 				case 4:
-					[descriptions appendFormat:@"%@%@", seperator, shortKeyID];
+					[descriptions appendFormat:@"%@%@", seperator, keyID];
 					break;
 				case 5:
-					[descriptions appendFormat:@"%@%@ (%@)", seperator, name, shortKeyID];
+					[descriptions appendFormat:@"%@%@ (%@)", seperator, name, keyID];
 					break;
 				case 6:
-					[descriptions appendFormat:@"%@%@ (%@)", seperator, email, shortKeyID];
+					[descriptions appendFormat:@"%@%@ (%@)", seperator, email, keyID];
 					break;
 				default:
-					[descriptions appendFormat:@"%@%@ <%@> (%@)", seperator, name, email, shortKeyID];
+					[descriptions appendFormat:@"%@%@ <%@> (%@)", seperator, name, email, keyID];
 					break;
 			}
 		} else {
-			[descriptions appendFormat:@"%@%@", seperator, key.shortKeyID];
+			[descriptions appendFormat:@"%@%@", seperator, showFingerprint ? [[GKFingerprintTransformer sharedInstance] transformedValue:key.fingerprint] : key.keyID];
 		}
 		
 		

Source/ActionController_Private.h
@@ -1,5 +1,5 @@
 /*
- Copyright © Roman Zechmeister, 2017
+ Copyright © Roman Zechmeister, 2018
  
  Diese Datei ist Teil von GPG Keychain.
  
@@ -19,19 +19,10 @@
 
 #import "Globales.h"
 
-@class GKPhotoPopoverController;
+@class GKPhotoPopoverController, SheetController;
 
 
 @interface ActionController ()
-
-typedef enum {
-	DescriptionNoKeyID = 8,
-	DescriptionNoEmail = 16,
-	DescriptionNoName = 32,
-	DescriptionSingleLine = 64,
-	DescriptionIndent = 128
-	
-} DescriptionOptions;
 
 
 @property (weak) IBOutlet NSArrayController *keysController;
@@ -46,13 +37,13 @@ typedef enum {
 @property (weak) IBOutlet GKPhotoPopoverController *photoPopoverController;
 @property (weak) IBOutlet NSPopover *photoPopover;
 
+@property (readonly) SheetController *sheetController;
 
 @property (strong) NSString *progressText, *errorText;
 @property (strong, readonly) NSUndoManager *undoManager;
 @property (strong) NSSet *revCertCache;
 
 - (void)receiveKeysFromServer:(NSObject <EnumerationList> *)keys;
-- (NSString *)descriptionForKeys:(NSObject <EnumerationList> *)keys maxLines:(NSUInteger)lines withOptions:(DescriptionOptions)options;
 
 - (BOOL)warningSheetWithDefault:(BOOL)defaultValue string:(NSString *)string, ...;
 

commit 0ac8f353615177d0fb0429d6058da218459d11ad

Fix a regression introduced in b354238.
[#454 state:fixed]

GPGKeychain.xcodeproj/project.pbxproj
@@ -11,6 +11,7 @@
 		3004A20B1CCF8948006D4E16 /* lock.icns in Resources */ = {isa = PBXBuildFile; fileRef = 3004A20A1CCF8948006D4E16 /* lock.icns */; };
 		301214B6196879A300D148B0 /* GKVolumeCollectionView.m in Sources */ = {isa = PBXBuildFile; fileRef = 301214B5196879A300D148B0 /* GKVolumeCollectionView.m */; };
 		3016CBBD1962E546000B42EA /* Keyring.png in Resources */ = {isa = PBXBuildFile; fileRef = 3016CBBC1962E546000B42EA /* Keyring.png */; };
+		3028BB2A20D113790001C648 /* GKFieldEditor.m in Sources */ = {isa = PBXBuildFile; fileRef = 3028BB2920D113790001C648 /* GKFieldEditor.m */; };
 		3029EC521E7FEFDD00B8ECAE /* GKPhotoPopoverController.m in Sources */ = {isa = PBXBuildFile; fileRef = 3029EC511E7FEFDD00B8ECAE /* GKPhotoPopoverController.m */; };
 		3036CB4118A55EC800BBC167 /* GKImageView.m in Sources */ = {isa = PBXBuildFile; fileRef = 3036CB4018A55EC800BBC167 /* GKImageView.m */; };
 		3045A759173D0623006E7FDE /* Libmacgpg.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 3045A758173D0623006E7FDE /* Libmacgpg.framework */; settings = {ATTRIBUTES = (Weak, ); }; };
@@ -98,6 +99,8 @@
 		3019D8EE1C0DB79D0003DE24 /* ja */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = ja; path = ja.lproj/Preferences.strings; sourceTree = "<group>"; };
 		3019D8EF1C0DB7AB0003DE24 /* ja */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = ja; path = ja.lproj/Localizable.strings; sourceTree = "<group>"; };
 		3023EABB144B77EC00AC13D9 /* ActionController_Private.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ActionController_Private.h; sourceTree = "<group>"; };
+		3028BB2820D113790001C648 /* GKFieldEditor.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = GKFieldEditor.h; sourceTree = "<group>"; };
+		3028BB2920D113790001C648 /* GKFieldEditor.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = GKFieldEditor.m; sourceTree = "<group>"; };
 		3029EC501E7FEFDD00B8ECAE /* GKPhotoPopoverController.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = GKPhotoPopoverController.h; sourceTree = "<group>"; };
 		3029EC511E7FEFDD00B8ECAE /* GKPhotoPopoverController.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = GKPhotoPopoverController.m; sourceTree = "<group>"; };
 		302AC96E1ECC593D0014330E /* Mail.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = Mail.h; sourceTree = "<group>"; };
@@ -350,6 +353,8 @@
 				30B29BED19116DF400FC453C /* GKScripting.m */,
 				30F8105F1AEF9709008B7857 /* GKDateCell.h */,
 				30F810601AEF9709008B7857 /* GKDateCell.m */,
+				3028BB2820D113790001C648 /* GKFieldEditor.h */,
+				3028BB2920D113790001C648 /* GKFieldEditor.m */,
 				30ECA2C8194C8AB800C1E761 /* GKAExtensions.h */,
 				30ECA2C9194C8AB800C1E761 /* GKAExtensions.m */,
 				3058A61B19D5B65D008568EA /* PLWeakCompatibilityStubs.h */,
@@ -616,6 +621,7 @@
 				3036CB4118A55EC800BBC167 /* GKImageView.m in Sources */,
 				30ECA2CA194C8AB800C1E761 /* GKAExtensions.m in Sources */,
 				30B29BEE19116DF400FC453C /* GKScripting.m in Sources */,
+				3028BB2A20D113790001C648 /* GKFieldEditor.m in Sources */,
 				301214B6196879A300D148B0 /* GKVolumeCollectionView.m in Sources */,
 				30EA818518A82E17000B4D0A /* GKTableView.m in Sources */,
 				3047D38E18B7C626001D9334 /* GKMenuButton.m in Sources */,

Source/AppDelegate.h
@@ -30,6 +30,7 @@
 @property (weak) IBOutlet NSTableView *userIDTable, *subkeyTable, *signatureTable;
 @property (weak) IBOutlet NSDrawer *drawer;
 @property (nonatomic) BOOL inspectorVisible;
+@property (nonatomic, strong) NSTextView *fieldEditor;
 
 
 - (IBAction)toggleInspector:(id)sender;

Source/AppDelegate.m
@@ -22,6 +22,7 @@
 #import "ActionController.h"
 #import "PreferencesController.h"
 #import "SBSystemPreferences.h"
+#import "GKFieldEditor.h"
 
 
 @implementation GPGKeychainAppDelegate
@@ -155,6 +156,7 @@
 - (id)init {
 	self = [super init];
 	appDelegate = self;
+	self.fieldEditor = [GKFieldEditor new];
 	return self;
 }
 
@@ -253,6 +255,10 @@
 
 - (NSUndoManager *)windowWillReturnUndoManager:(NSWindow *)awindow {
 	return [[ActionController sharedInstance] undoManager];
+}
+
+- (id)windowWillReturnFieldEditor:(NSWindow *)sender toObject:(id)client {
+	return self.fieldEditor;
 }
 
 

Source/GKFieldEditor.h
@@ -1,0 +1,9 @@
+//
+//  GKFieldEditor.h
+//  GPG Keychain
+//
+//  Created by Mento on 13.06.18.
+//
+
+@interface GKFieldEditor : NSTextView
+@end

Source/GKFieldEditor.m
@@ -1,0 +1,33 @@
+//
+//  GKFieldEditor.m
+//  GPG Keychain
+//
+//  Created by Mento on 13.06.18.
+//
+
+#import "GKFieldEditor.h"
+
+@implementation GKFieldEditor
+
+- (BOOL)writeSelectionToPasteboard:(NSPasteboard *)pboard type:(NSPasteboardType)type {
+	if ([type isEqualToString:NSStringPboardType]) {
+		NSMutableString *selectedString = [NSMutableString new];
+		NSString *string = self.string;
+		NSArray <NSValue *> *ranges = self.selectedRanges;
+		
+		for (NSValue *rangeValue in ranges) {
+			NSRange range = rangeValue.rangeValue;
+			[selectedString appendString:[string substringWithRange:range]];
+		}
+		
+		[selectedString replaceOccurrencesOfString:@"\xC2\xA0" withString:@" " options:0 range:NSMakeRange(0, selectedString.length)];
+		
+		return [pboard setString:selectedString forType:NSStringPboardType];
+	}
+
+	return [super writeSelectionToPasteboard:pboard type:type];
+}
+
+@end
+
+

Source/Globales.m
@@ -158,8 +158,8 @@ NSString *filenameForExportedKeys(NSArray *keys, NSString **secFilename) {
 		transformed = [super transformedValue:value];
 	}
 	
-	transformed = [transformed stringByReplacingOccurrencesOfString:@"  " withString:@"\xC2\xA0"];
-	transformed = [transformed stringByReplacingOccurrencesOfString:@" " withString:@"\xE2\x80\xAF"];
+	transformed = [transformed stringByReplacingOccurrencesOfString:@"  " withString:@"\xC2\xA0\xC2\xA0"];
+	transformed = [transformed stringByReplacingOccurrencesOfString:@" " withString:@"\xC2\xA0"];
 	return transformed;
 }
 + (id)sharedInstance {