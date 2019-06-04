8dadbb96aca6781e74095c3e894448ec52efcd64
Mento <Mento@gpgtools.org>
2019-06-04 11:46:26 +0200

    Replace toolbar icons with new vector icons
    [#481]

--- Source/main.m
+++ Source/main.m
@@ -2,13 +2,6 @@
 #import <Libmacgpg/Libmacgpg.h>
 #import "Globales.h"
 
-void prepareDarkModeImages(void);
-
-@interface NSAppearance(bestMatchFromAppearancesWithNames)
-- (NSAppearanceName)bestMatchFromAppearancesWithNames:(NSArray<NSAppearanceName> *)appearances;
-@end
-
-
 
 int main(int argc, const char *argv[]) {
 	if (![GPGController class]) {
@@ -22,39 +15,7 @@ int main(int argc, const char *argv[]) {
         return 1;
     }
 #endif
-	prepareDarkModeImages();
 	
     return NSApplicationMain(argc, argv);
-}
-
-
-void prepareDarkModeImages() {
-	if (@available(macOS 10.14, *)) {
-		NSArray *imageNames = @[@"Search"];
-		
-		for (NSString *imageName in imageNames) {
-			
-			NSImage *lightImage = [NSImage imageNamed:imageName];
-			lightImage.name = [imageName stringByAppendingString:@"_light"];
-			NSImage *darkImage = [NSImage imageNamed:[imageName stringByAppendingString:@"_dark"]];
-			
-			NSImage *searchImage = [NSImage imageWithSize:lightImage.size flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
-				NSAppearance *appearance = NSAppearance.currentAppearance;
-				NSAppearanceName appearanceName = [appearance bestMatchFromAppearancesWithNames:@[NSAppearanceNameAqua, @"NSAppearanceNameDarkAqua"]];
-				
-				NSImage *image;
-				if ([appearanceName isEqualToString:@"NSAppearanceNameDarkAqua"]) {
-					image = darkImage;
-				} else {
-					image = lightImage;
-				}
-				[image drawInRect:dstRect];
-				return YES;
-			}];
-			
-			[searchImage setName:imageName];
-		}
-		
-	}
 }
 
